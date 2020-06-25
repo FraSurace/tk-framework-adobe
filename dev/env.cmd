69a2e30847a63c536e8cea5d9a8080cf4d782551
Francesco <fra.surace@gmail.com>
2020-06-25 23:54:24 +0200

    fix conflict

--- dev/env.cmd
+++ dev/env.cmd
@@ -3,26 +3,16 @@ rem set this to the target version you want to set this to
 set "TARGET_VERSION=1.0.4"
 
 rem set this to the core you are working with. eg:0.18.160
-<<<<<<< HEAD
 set "TKCORE_VERSION=0.19.11"
 rem if you want to use tk-core from a non default bundle-cache location,
 rem uncomment the following line and set your path accordingly.
-set "TKCORE_FOLDER=%APPDATA%\Shotgun\bundle_cache\app_store\tk-core"
+rem set "TKCORE_FOLDER=%APPDATA%\Shotgun\bundle_cache\app_store\tk-core"
 
 set "ZXP_SIGN_TOOL=C:\Users\francesco\Documents\GitHub\tk-framework-adobe\dev\ZXPSignCmd.exe"
-=======
-set "TKCORE_VERSION=0.18.160"
-rem if you want to use tk-core from a non default bundle-cache location,
-rem uncomment the following line and set your path accordingly.
-rem set "TKCORE_FOLDER="
-
-set "ZXP_SIGN_TOOL="
->>>>>>> parent of 3581595... fix adobe framework for premiere
 set "PYTHON_EXE=python"
 
 rem DO NOT COMMIT THE FOLLOWING LINES TO THE REPO
 rem set this to your certificate file, that you created / will create
-<<<<<<< HEAD
 set "CERTIFICATE_FILE="C:\Users\francesco\Documents\GitHub\tk-framework-adobe\dev\certificate"
 rem set this to the password that you chose/want to chose for your certificate file
 rem the following options are needed, when you want to create a new certificate
@@ -31,16 +21,6 @@ set "CERT_COUNTRY=VLC"
 set "CERT_STATE=Spain"
 set "CERT_ORG=La Tribu Animation S.L."
 set "CERT_CN=000000"
-=======
-set "CERTIFICATE_FILE="
-rem set this to the password that you chose/want to chose for your certificate file
-rem the following options are needed, when you want to create a new certificate
-set "CERTIFICATE_PASS="
-set "CERT_COUNTRY="
-set "CERT_STATE="
-set "CERT_ORG="
-set "CERT_CN="
->>>>>>> parent of 3581595... fix adobe framework for premiere
 
 rem optional certificate fields (uncomment if desired)
 rem set "CERT_LOCALITY="
