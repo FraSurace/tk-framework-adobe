d441bb99bd5259c2106879f8bde636bfa1676d39
Francesco <fra.surace@gmail.com>
2020-06-25 22:50:03 +0200

    wip framework adobe

--- dev/env.cmd
+++ dev/env.cmd
@@ -3,7 +3,7 @@ rem set this to the target version you want to set this to
 set "TARGET_VERSION=0.0.1"
 
 rem set this to the core you are working with. eg:0.18.160
-rem set "TKCORE_VERSION=v0.19.11"
+rem set "TKCORE_VERSION=0.19.11"
 rem if you want to use tk-core from a non default bundle-cache location,
 rem uncomment the following line and set your path accordingly.
 rem set "TKCORE_FOLDER=../../../app_store\\tk-core"
